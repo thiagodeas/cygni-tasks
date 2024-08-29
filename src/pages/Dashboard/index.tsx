b5bb4cddd17e92e4680ba113bb46f1e37195a248
thiagodeas <sousaalvesth@gmail.com>
2024-08-28 22:06:05 -0300

    iniciando a criacao da tela principal da aplicacao

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,17 @@
 export const Dashboard = () => {
-    return (
-        <div>
-            <h1 className="text-main-title">Login feito com sucesso!</h1>
-        </div>
-    );
-}
+  return (
+    <div className="bg-[#290D34] w-screen h-screen flex flex-col items-center justify-center gap-8">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-custom-title text-galactic-glow text-center font-semibold">
+          Lista de tarefas semanal
+        </h1>
+      </div>
+
+      <div className="w-[1100px] h-[400px] border-2 rounded-lg border-stellar-lavender">
+
+
+      </div>
+
+    </div>
+  );
+};
