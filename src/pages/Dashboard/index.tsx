fb7df4773399a5e3afce1badda51f5753d7d434c
thiagodeas <sousaalvesth@gmail.com>
2024-09-03 18:54:20 -0300

    adicionando logica de criacao de task

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,37 @@
 import { useState } from "react";
+import { IoPlanetOutline } from "react-icons/io5";
+import { api } from "../../api";
 
 export const Dashboard: React.FC = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [task, setTask] = useState("");
 
-  const toggleExpansion = () => {
-    setIsExpanded(!isExpanded);
-  };
+  const createTask = async () => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      console.error("Token não encontrado!");
+      return;
+    }
+
+    try {
+      const response = await api.post('/task', {
+        description: task,
+      },
+      {headers:{
+        Authorization: `Bearer ${token}`,
+      },
+      }
+      );
+
+      if (response.status === 201) {
+        console.log("Tarefa criada com sucesso!");
+        setTask("");
+      }
+
+    } catch (error) {
+      console.error("Não foi possível criar nova tarefa ", error);
+    }
+  }
 
   return (
     <div className="bg-[#290D34] w-screen h-screen flex flex-col items-center justify-center gap-8">
@@ -15,25 +41,27 @@ export const Dashboard: React.FC = () => {
 
       <div
         className={`flex ${
-          isExpanded ? " flex-col " : " flex-row "
-        }items-center justify-start border-2 rounded-lg border-stellar-lavender transition-all ease-in-out duration-700 w-[800px] ${
-          isExpanded ? "h-[280px]" : "h-[50px]"
+          task ? " flex-col " : " flex-row "
+        }items-center justify-start border-[1px] rounded-lg bg-transparent border-cloudy-rose transition-all ease-in-out duration-700 w-[800px] ${
+          task ? "h-[280px]" : "h-[50px]"
         }`}
       >
         <input
           type="text"
-          placeholder="Adicionar uma nova tarefa"
-          className="bg-transparent outline-none w-full h-10 py-12 text-galactic-glow text-center text-options-title placeholder: tracking-wide"
-          onClick={toggleExpansion}
+          placeholder="Digite uma nova tarefa"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          required
+          className="bg-transparent outline-none w-full h-10 py-10 font-light text-galactic-glow text-center text-options-title placeholder: tracking-wide placeholder:text-galactic-glow placeholder:font-light"
         />
 
-        {isExpanded && (
-          <div className="flex flex-col items-center justify-start w-full text-galactic-glow h-[50px] transition-all ease-in-out duration-700 px-12">
-            <div className="flex items-center justify-between w-full">
-              <span className="p-2 text-options-title">Prioridade</span>
+        {task && (
+          <div className="flex flex-col items-center justify-start w-full text-galactic-glow h-[50px] transition-all ease-in-out duration-700">
+            <div className="flex items-center justify-center w-full border-cloudy-rose border-y-[1px] py-2 gap-32">
+              <span className="p-2 text-options-title pr-8">Prioridade</span>
               <div className="flex gap-2">
                 <input type="radio" name="priority" className="w-4" />
-                Alta
+                Baixa
               </div>
               <div className="flex gap-2">
                 <input type="radio" name="priority" className="w-4" />
@@ -41,10 +69,15 @@ export const Dashboard: React.FC = () => {
               </div>
               <div className="flex gap-2">
                 <input type="radio" name="priority" className="w-4" />
-                Baixa
+                Alta
               </div>
             </div>
-            <button className="mt-12 p-2 border-stellar-lavender border-2 rounded-lg">Adicionar tarefa</button>
+            <button
+            className="flex items-center justify-center mt-12 gap-x-2 p-2 w-48 rounded-lg bg-background-grad font-semibold text-white text-center text-[1.1rem] hover:scale-110 transition-all ease-in-out duration-700"
+            onClick={createTask}
+            >Adicionar tarefa 
+            <IoPlanetOutline className="text-[1.6rem]" />
+            </button>
           </div>
         )}
       </div>
